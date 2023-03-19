595de549b909f1fa433e6cdd6f91126de0b18b92
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2023-03-18 20:41:31 -0400

    Adding docs - smart contracts

--- packages/backend/contracts/MyToken.sol
+++ packages/backend/contracts/MyToken.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import the OpenZeppelin ERC20 contract
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// MyToken contract that inherits the OpenZeppelin ERC20 contract
 contract MyToken is ERC20 {
+
+    // Constructor for the MyToken contract
+    // Takes an initial supply and a destination address as parameters
     constructor(uint256 initialSupply, address destination) ERC20("My Token", "MTK") {
-        _mint(msg.sender, initialSupply);
+    // Mint the initial supply of tokens and assign them to the destination address
+    _mint(destination, initialSupply);
+
     }
-}
+}   
