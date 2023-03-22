b0814108775b3bfeb68f6ec583985dbab200f22f
Turja Chowdhury <42366823+tchowd@users.noreply.github.com>
2023-03-22 17:34:50 -0400

    update docs - complete

--- packages/backend/deploy/deploy.ts
+++ packages/backend/deploy/deploy.ts
@@ -1,8 +1,10 @@
+// Import necessary libraries and types
 import { Wallet, utils } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
+// Retrieve the private key from environment variables
 const PRIVATE_KEY: string = process.env.ZKS_PRIVATE_KEY || "";
 if (!PRIVATE_KEY) {
   throw new Error("Please set ZKS_PRIVATE_KEY in the environment variables.");
@@ -11,55 +13,50 @@ if (!PRIVATE_KEY) {
 export default async function (hre: HardhatRuntimeEnvironment) {
   console.log(`Running deploy script for the contracts`);
 
-  // Initialize the wallet.
+  // Initialize the zkSync wallet with the private key
   const wallet = new Wallet(PRIVATE_KEY);
 
-  // Create deployer object and load the artifact of the contract you want to deploy.
+  // Create a deployer object for deploying contracts using the zkSync-web3 library
   const deployer = new Deployer(hre, wallet);
 
   const initialSupply = 100000;
   const address = deployer.zkWallet.address;
 
+  // Generate a random number between 0 - 10 for the easy game and hash it
   const randomNumberEasy: number = Math.floor(Math.random() * 11);
   console.log(randomNumberEasy);
   const hashEasy = ethers.utils.solidityKeccak256(["uint256"], [randomNumberEasy])
 
+  // Generate a random number between 0 - 100 for the hard game and hash it
   const randomNumberHard: number = Math.floor(Math.random() * 100);
   console.log(randomNumberHard);
   const hashHard = ethers.utils.solidityKeccak256(["uint256"], [randomNumberHard])
-  
+
+  // Load the contract artifacts for the easy and hard guess game contracts, and the token contract
   const guessGameArtifactEasy = await deployer.loadArtifact("GuessTheNumberGameEasy");
   const guessGameArtifactHard = await deployer.loadArtifact("GuessTheNumberGameHard");
   const myTokenArtifact = await deployer.loadArtifact("MyToken");
-  const converToHashArtifact = await deployer.loadArtifact("ConvertToHash");
 
+  // Deploy the easy and hard guess game contracts, and the token contract
   const guessTheNumberGameEasy = await deployer.deploy(guessGameArtifactEasy, [hashEasy]);
   const guessTheNumberGameHard = await deployer.deploy(guessGameArtifactHard, [hashHard]);
   const token = await deployer.deploy(myTokenArtifact, [initialSupply, address]);
-  const conversion = await deployer.deploy(converToHashArtifact);
 
-  // Show the contract info.
+  // Log the deployment information for the easy guess game contract
   const numGameAddressEasy = guessTheNumberGameEasy.address;
   console.log(`${guessGameArtifactEasy.contractName} was deployed to ${numGameAddressEasy}`);
   const deploymentFeeGuessNumGameEasy = await deployer.estimateDeployFee(guessGameArtifactEasy, [hashEasy]);
   const parsedFeeGuessNumGameEasy = ethers.utils.formatEther(deploymentFeeGuessNumGameEasy.toString());
   console.log(`The deployment is estimated to cost ${parsedFeeGuessNumGameEasy} ETH`);
 
+  // Log the deployment information for the hard guess game contract
   const numGameAddressHard = guessTheNumberGameHard.address;
   console.log(`${guessGameArtifactHard.contractName} was deployed to ${numGameAddressHard}`);
   const deploymentFeeGuessNumGameHard = await deployer.estimateDeployFee(guessGameArtifactHard, [hashHard]);
   const parsedFeeGuessNumGameHard = ethers.utils.formatEther(deploymentFeeGuessNumGameHard.toString());
   console.log(`The deployment is estimated to cost ${parsedFeeGuessNumGameHard} ETH`);
 
+  // Log the deployment information for the token contract
   const tokenAddress = token.address;
   console.log(`${myTokenArtifact.contractName} was deployed to ${tokenAddress}`);
-  // const deploymentFeeTokenAddress = await deployer.estimateDeployFee(myTokenArtifact, [labelhash]);
-  // const parsedFeeTokenAddress = ethers.utils.formatEther(deploymentFeeTokenAddress.toString());
-  // console.log(`The deployment is estimated to cost ${parsedFeeTokenAddress} ETH`);
-
-  const conversionAddress = conversion.address;
-  console.log(`${converToHashArtifact.contractName} was deployed to ${conversionAddress}`);
-  // const deploymentFeeConversionAddress = await deployer.estimateDeployFee(converToHashArtifact, [labelhash]);
-  // const parsedFeeConversionAddress = ethers.utils.formatEther(deploymentFeeConversionAddress.toString());
-  // console.log(`The deployment is estimated to cost ${parsedFeeConversionAddress} ETH`);
 }
